41cd13a3db6d6e18b58ee4773eba2bb014621efd
Anton Balynov <devchoppa@gmail.com>
2023-07-20 14:20:48 +0300

    Update test.js

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,7 +2,7 @@ import path from 'path';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
 import gendiff from '../src/index.js';
-import readFile from '../src/readFile.js';
+// import readFile from '../src/readFile.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
