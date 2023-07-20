4527246d5357e613949f811dd580d4fae7db0e4a
Anton Balynov <devchoppa@gmail.com>
2023-07-20 13:08:48 +0300

    Add new test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,5 +1,17 @@
-import { test, expect } from '@jest/globals';
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import gendiff from '../src/index.js';
+import readFile from '../src/readFile.js';
 
-test('test', () => {
-  expect(2 + 2).toBe(4);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readContent = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
+test('Diff test json', () => {
+  const fileDiff = gendiff('./__fixtures__/file1.json', './__fixtures__/file2.json');
+  const expectedFile = 'expected1.txt';
+  expect(fileDiff).toEqual(readContent(expectedFile));
 });
