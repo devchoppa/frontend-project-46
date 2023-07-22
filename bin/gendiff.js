03045f0ff9f52f99c61a96380da9cab917b95200
Anton Balynov <devchoppa@gmail.com>
2023-07-22 18:17:42 +0300

    Add option.format

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -11,8 +11,7 @@ program
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format <type>', 'output format (default "stylish")')
   .action((filepath1, filepath2) => {
-    const options = program.opts();
-    console.log(gendiff(filepath1, filepath2, options.format));
+    console.log(gendiff(filepath1, filepath2, program.opts().format));
   });
 
 program.parse();
