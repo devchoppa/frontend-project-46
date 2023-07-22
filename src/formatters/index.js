ca59388bca56b476bc619af84936e02c6005d08d
Anton Balynov <devchoppa@gmail.com>
2023-07-22 18:18:03 +0300

    Add new formats

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,9 +1,15 @@
 import stylishFormat from './stylish.js';
+import plainFormat from './plain.js';
+import jsonFormat from './json.js';
 
 const selectFormat = (resultTree, format) => {
   switch (format) {
     case 'stylish':
       return stylishFormat(resultTree);
+    case 'plain':
+      return plainFormat(resultTree);
+    case 'json':
+      return jsonFormat(resultTree);
     default: throw new Error(`Unknown format:${format}`);
   }
 };
