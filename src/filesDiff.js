7c2e43e8145113f0ce4edd063358c0e1e6a323fd
Anton Balynov <devchoppa@gmail.com>
2023-07-22 14:52:35 +0300

    Update function

--- src/filesDiff.js
+++ src/filesDiff.js
@@ -1,9 +1,9 @@
 import _ from 'lodash';
 
-const getTreeObject = (obj1, obj2) => {
+const buildTree = (obj1, obj2) => {
   const keysSorted = _.sortBy(Object.keys({ ...obj1, ...obj2 }));
-
   const treeObject = keysSorted.map((key) => {
+    if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) return { state: 'nodes', name: key, children: buildTree(obj1[key], obj2[key]) };
     if (!_.has(obj1, key)) return { state: 'added', name: key, value: obj2[key] };
     if (!_.has(obj2, key)) return { state: 'deleted', name: key, value: obj1[key] };
     if (obj1[key] !== obj2[key]) {
@@ -14,30 +14,4 @@ const getTreeObject = (obj1, obj2) => {
   return treeObject;
 };
 
-const proccesDiff = (object) => {
-  const resultDif = object.map((key) => {
-    let result = '';
-
-    switch (key.state) {
-      case 'unchanged':
-        result += `    ${key.name}: ${key.value}`;
-        break;
-      case 'deleted':
-        result += `  - ${key.name}: ${key.value}`;
-        break;
-      case 'added':
-        result += `  + ${key.name}: ${key.value}`;
-        break;
-      case 'changed':
-        result += `  - ${key.name}: ${key.value.oldValue}\n  + ${key.name}: ${key.value.newValue}`;
-        break;
-      default: break;
-    }
-
-    return result;
-  });
-
-  return `{\n${resultDif.join('\n')}\n}`;
-};
-
-export { getTreeObject, proccesDiff };
+export default buildTree;
