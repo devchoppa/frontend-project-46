510e6c5656c07517ac1edf419ccb5e6ba5ddb969
Anton Balynov <devchoppa@gmail.com>
2023-07-22 14:51:29 +0300

    Update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,9 +9,10 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1', '-V, --version', 'output the version number')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format (default "stylish")')
   .action((filepath1, filepath2) => {
-    console.log(gendiff(filepath1, filepath2));
+    const options = program.opts();
+    console.log(gendiff(filepath1, filepath2, options.format));
   });
 
 program.parse();
