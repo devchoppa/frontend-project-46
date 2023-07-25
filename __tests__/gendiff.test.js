12212c1d356e26ad512c067fef6cd7a516cffd99
Anton Balynov <devchoppa@gmail.com>
2023-07-25 21:43:18 +0300

    Update tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -10,16 +10,16 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 const readContent = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 const expectedStylish = readContent('expectedStylish.txt');
-// const expectedPlain = readContent('expectedPlain.txt');
-// const expectedJson = readContent('expectedJson.txt');
+const expectedPlain = readContent('expectedPlain.txt');
+const expectedJson = readContent('expectedJson.txt');
 
 const testFormats = ['json', 'yml'];
 describe('gendiff', () => {
   test.each(testFormats)('%s', (format) => {
     const filepath1 = getFixturePath(`filepath1.${format}`);
     const filepath2 = getFixturePath(`filepath2.${format}`);
-    expect(gendiff(filepath1, filepath2)).toEqual(expectedStylish);
-    // expect(gendiff(filepath1, filepath2)).toEqual(expectedPlain);
-    // expect(gendiff(filepath1, filepath2)).toEqual(expectedJson);
+    expect(gendiff(filepath1, filepath2, 'stylish')).toEqual(expectedStylish);
+    expect(gendiff(filepath1, filepath2, 'plain')).toEqual(expectedPlain);
+    expect(gendiff(filepath1, filepath2, 'json')).toEqual(expectedJson);
   });
 });
