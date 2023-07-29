8a1cafca01cc8517520c854ea0d974ff64fb4d03
Anton Balynov <devchoppa@gmail.com>
2023-07-29 18:54:37 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import fs from 'fs';
 import path from 'path';
-import buildTree from './filediff.js';
-import selectFormat from './formatters/index.js';
+import buildTree from './buildTree.js';
+import format from './formatters/index.js';
 import parse from './parser.js';
 
-const getAbsPath = (filepath) => path.resolve(process.cwd(), filepath);
-const readFile = (filepath) => fs.readFileSync(getAbsPath(filepath), 'utf-8');
-const getFormat = (filepath) => path.extname(filepath).slice(1);
+const readFile = (filepath) => {
+  const fullPath = path.resolve(process.cwd(), filepath);
+  const data = fs.readFileSync(fullPath).toString();
+  return parse(data, path.extname(filepath).slice(1));
+};
 
-const gendiff = (filepath1, filepath2, format = 'stylish') => {
-  const readContent1 = readFile(filepath1);
-  const readContent2 = readFile(filepath2);
-  const data1 = parse(readContent1, getFormat(filepath1));
-  const data2 = parse(readContent2, getFormat(filepath2));
-  const treeObject = buildTree(data1, data2);
-  return selectFormat(treeObject, format);
+const gendiff = (filepath1, filepath2, outputFormat = 'stylish') => {
+  const data1 = readFile(filepath1);
+  const data2 = readFile(filepath2);
+  const tree = buildTree(data1, data2);
+  return format(tree, outputFormat);
 };
 
 export default gendiff;
