b944e20f20309a02af04a7322f6fae305df95af6
Anton Balynov <devchoppa@gmail.com>
2023-07-29 18:53:48 +0300

    Update filename

---
+++ src/buildTree.js
@@ -0,0 +1,16 @@
+import _ from 'lodash';
+
+const buildTree = (obj1, obj2) => {
+  const sortedKeys = _.sortBy(Object.keys({ ...obj1, ...obj2 }));
+  return sortedKeys.map((key) => {
+    if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) return { state: 'nodes', name: key, children: buildTree(obj1[key], obj2[key]) };
+    if (!_.has(obj1, key)) return { state: 'added', name: key, value: obj2[key] };
+    if (!_.has(obj2, key)) return { state: 'deleted', name: key, value: obj1[key] };
+    if (obj1[key] !== obj2[key]) {
+      return { state: 'changed', name: key, value: { oldValue: obj1[key], newValue: obj2[key] } };
+    }
+    return { state: 'unchanged', name: key, value: obj2[key] };
+  });
+};
+
+export default buildTree;
