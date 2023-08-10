034b3e2c05d7f026ecfebb5f4a38a93d59d00c76
Anton Balynov <devchoppa@gmail.com>
2023-08-10 12:24:27 +0300

    Update stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,35 +1,36 @@
 import _ from 'lodash';
 
-const indent = (depth) => ' '.repeat((depth * 4) - 2);
+const countMargins = 4;
+const offsetLeft = 2;
+
+const indent = (depth) => ' '.repeat((depth * countMargins) - offsetLeft);
 
 const stringify = (value, depth) => {
   if (!_.isObject(value)) {
     return value;
   }
   const keys = Object.keys(value);
-  const getKeys = keys.map((key) => `${indent(depth + 1)}  ${key}: ${stringify(value[key], depth + 1)}`);
-  return `{\n${getKeys.join('\n')}\n  ${indent(depth)}}`;
+  const getOutput = keys.map((key) => `${indent(depth + 1)}  ${key}: ${stringify(value[key], depth + 1)}`);
+  return `{\n${getOutput.join('\n')}\n  ${indent(depth)}}`;
 };
+const iter = (tree, depth) => tree.map((node) => {
+  const createString = (value, sign) => `${indent(depth)}${sign} ${node.key}: ${stringify(value, depth)}\n`;
+  switch (node.type) {
+    case 'added':
+      return createString(node.value2, '+');
+    case 'deleted':
+      return createString(node.value, '-');
+    case 'unchanged':
+      return createString(node.value2, ' ');
+    case 'changed':
+      return `${createString(node.value1, '-')}${createString(node.value2, '+')}`;
+    case 'nodes':
+      return `${indent(depth)}  ${node.key}: {\n${iter(node.children, depth + 1).join('')}${indent(depth)}  }\n`;
+    default:
+      throw new Error(`This type does not exist: ${node.type}`);
+  }
+});
 
-const stylishFormat = (mainTree) => {
-  const iter = (tree, depth) => tree.map((node) => {
-    const createString = (value, sign) => `${indent(depth)}${sign} ${node.key}: ${stringify(value, depth)}\n`;
-    switch (node.type) {
-      case 'added':
-        return createString(node.value2, '+');
-      case 'deleted':
-        return createString(node.value1, '-');
-      case 'unchanged':
-        return createString(node.value2, ' ');
-      case 'changed':
-        return `${createString(node.value1, '-')}${createString(node.value2, '+')}`;
-      case 'nodes':
-        return `${indent(depth)}  ${node.key}: {\n${iter(node.children, depth + 1).join('')}${indent(depth)}  }\n`;
-      default:
-        throw new Error(`This type does not exist: ${node.type}`);
-    }
-  });
-  return `{\n${iter(mainTree, 1).join('')}}`;
-};
+const getFormatStylish = (tree) => `{\n${iter(tree, 1).join('')}}`;
 
-export default stylishFormat;
+export default getFormatStylish;
